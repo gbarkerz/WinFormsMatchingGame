474ca1a2fb20954e8b134808ae47a8778b9d7a1e
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2021-11-03 17:44:25 +0000

    Add project files.

---
+++ WinFormsMatchingGame/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace WinFormsMatchingGame
+{
+    partial class FormMatchingGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormMatchingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 536);
+            this.Name = "FormMatchingGame";
+            this.Text = "Accessible Matching Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WinFormsMatchingGame/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsMatchingGame
+{
+    public partial class FormMatchingGame : Form
+    {
+        private DataGridView dataGridView = new DataGridView();
+
+        public FormMatchingGame()
+        {
+            InitializeComponent();
+
+            dataGridView.AccessibilityObject.Name = "Content for matching";
+
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.ColumnHeadersVisible = false;
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.ShowCellToolTips = false;
+
+            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+
+            dataGridView.Rows.Add();
+            dataGridView.Rows.Add();
+            dataGridView.Rows.Add();
+            dataGridView.Rows.Add();
+
+            dataGridView.CellClick += DataGridView_CellClick;
+            dataGridView.SizeChanged += Grid_SizeChanged;
+
+            this.Controls.Add(dataGridView);
+
+            ResizeGridContent();
+        }
+
+        private void Grid_SizeChanged(object sender, EventArgs e)
+        {
+            ResizeGridContent();
+        }
+
+        private void ResizeGridContent()
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                dataGridView.Columns[i].Width = (this.ClientSize.Width / 4) - 1;
+                dataGridView.Rows[i].Height = (this.ClientSize.Height / 4) - 1;
+            }
+        }
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+    }
+
+    public class DataGridViewButtonColumnWithCustomName : DataGridViewButtonColumn
+    {
+        public DataGridViewButtonColumnWithCustomName()
+        {
+            this.CellTemplate = new DataGridViewButtonCellWithCustomName();
+        }
+    }
+
+    public class DataGridViewButtonCellWithCustomName : DataGridViewButtonCell
+    {
+        protected override AccessibleObject CreateAccessibilityInstance()
+        {
+            return new DataGridViewButtonCellWithCustomNameAccessibleObject(this);
+        }
+
+        protected class DataGridViewButtonCellWithCustomNameAccessibleObject : 
+            DataGridViewButtonCellAccessibleObject
+        {
+            public DataGridViewButtonCellWithCustomNameAccessibleObject(DataGridViewButtonCellWithCustomName owner) : base(owner)
+            {
+            }
+
+            public override string Name
+            {
+                get
+                {
+                    // Add the custom accessible name, and follow that with the row information.
+                    //return Resources.ResourceManager.GetString("ImageColumnAccessibleName") + base.Name;
+                    return "Unknown";
+                }
+            }
+
+            // This didn't impact the UIA ControlType, which was still a Button.
+            //public override AccessibleRole Role
+            //{
+            //    get
+            //    {
+            //        return AccessibleRole.Cell;
+            //    }
+            //}
+        }
+    }
+}
