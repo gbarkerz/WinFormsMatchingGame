bce51ae67d1d2e762443de656fdc99d3948bd60c
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2021-11-07 10:01:21 +0000

    Add logic for clicking on the cards.

--- WinFormsMatchingGame/Form1.Designer.cs
+++ WinFormsMatchingGame/Form1.Designer.cs
@@ -29,13 +29,66 @@ namespace WinFormsMatchingGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelCardGrid = new System.Windows.Forms.Panel();
+            this.buttonTryAgain = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonRestartGame = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // panelCardGrid
+            // 
+            this.panelCardGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelCardGrid.Location = new System.Drawing.Point(13, 13);
+            this.panelCardGrid.Name = "panelCardGrid";
+            this.panelCardGrid.Size = new System.Drawing.Size(551, 452);
+            this.panelCardGrid.TabIndex = 0;
+            // 
+            // buttonTryAgain
+            // 
+            this.buttonTryAgain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonTryAgain.Enabled = false;
+            this.buttonTryAgain.Location = new System.Drawing.Point(13, 484);
+            this.buttonTryAgain.Name = "buttonTryAgain";
+            this.buttonTryAgain.Size = new System.Drawing.Size(131, 40);
+            this.buttonTryAgain.TabIndex = 1;
+            this.buttonTryAgain.Text = "&Try Again";
+            this.buttonTryAgain.UseVisualStyleBackColor = true;
+            this.buttonTryAgain.Click += new System.EventHandler(this.buttonTryAgain_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(432, 484);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(131, 40);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonRestartGame
+            // 
+            this.buttonRestartGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRestartGame.Location = new System.Drawing.Point(295, 484);
+            this.buttonRestartGame.Name = "buttonRestartGame";
+            this.buttonRestartGame.Size = new System.Drawing.Size(131, 40);
+            this.buttonRestartGame.TabIndex = 3;
+            this.buttonRestartGame.Text = "&Restart";
+            this.buttonRestartGame.UseVisualStyleBackColor = true;
+            this.buttonRestartGame.Click += new System.EventHandler(this.buttonRestartGame_Click);
             // 
             // FormMatchingGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 536);
+            this.Controls.Add(this.buttonRestartGame);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonTryAgain);
+            this.Controls.Add(this.panelCardGrid);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
             this.Name = "FormMatchingGame";
             this.Text = "Accessible Matching Game";
             this.ResumeLayout(false);
@@ -43,6 +96,11 @@ namespace WinFormsMatchingGame
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelCardGrid;
+        private System.Windows.Forms.Button buttonTryAgain;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonRestartGame;
     }
 }
 
--- WinFormsMatchingGame/Form1.cs
+++ WinFormsMatchingGame/Form1.cs
@@ -1,42 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace WinFormsMatchingGame
 {
     public partial class FormMatchingGame : Form
     {
-        private DataGridView dataGridView = new DataGridView();
+        private CardMatchingGrid cardMatchingGrid = new CardMatchingGrid();
+
+        private DataGridViewButtonCellWithCustomName firstCardInPairAttempt;
+        private DataGridViewButtonCellWithCustomName secondCardInPairAttempt;
 
         public FormMatchingGame()
         {
             InitializeComponent();
 
-            dataGridView.AccessibilityObject.Name = "Cards for matching";
+            cardMatchingGrid.CardList = new List<Card>()
+            {
+                new Card { Name = "A", },
+                new Card { Name = "A", },
+                new Card { Name = "B", },
+                new Card { Name = "B", },
+                new Card { Name = "C", },
+                new Card { Name = "C", },
+                new Card { Name = "D", },
+                new Card { Name = "D", },
+                new Card { Name = "E", },
+                new Card { Name = "E", },
+                new Card { Name = "F", },
+                new Card { Name = "F", },
+                new Card { Name = "G", },
+                new Card { Name = "G", },
+                new Card { Name = "H", },
+                new Card { Name = "H", },
+            };
 
-            dataGridView.RowHeadersVisible = false;
-            dataGridView.ColumnHeadersVisible = false;
-            dataGridView.Dock = DockStyle.Fill;
-            dataGridView.SelectionMode = DataGridViewSelectionMode.CellSelect;
-            dataGridView.AllowUserToAddRows = false;
-            dataGridView.ShowCellToolTips = false;
+            cardMatchingGrid.AccessibilityObject.Name = "Cards for matching";
 
-            // Todo: Should the grid be declared as ReadOnly here?
-            //dataGridView.ReadOnly = true;
+            cardMatchingGrid.RowHeadersVisible = false;
+            cardMatchingGrid.ColumnHeadersVisible = false;
+            cardMatchingGrid.Dock = DockStyle.Fill;
+            cardMatchingGrid.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            cardMatchingGrid.AllowUserToAddRows = false;
+            cardMatchingGrid.ShowCellToolTips = false;
 
-            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
-            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
-            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
-            dataGridView.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            cardMatchingGrid.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            cardMatchingGrid.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            cardMatchingGrid.Columns.Add(new DataGridViewButtonColumnWithCustomName());
+            cardMatchingGrid.Columns.Add(new DataGridViewButtonColumnWithCustomName());
 
-            dataGridView.Rows.Add();
-            dataGridView.Rows.Add();
-            dataGridView.Rows.Add();
-            dataGridView.Rows.Add();
+            cardMatchingGrid.Rows.Add();
+            cardMatchingGrid.Rows.Add();
+            cardMatchingGrid.Rows.Add();
+            cardMatchingGrid.Rows.Add();
 
-            dataGridView.CellClick += DataGridView_CellClick;
-            dataGridView.SizeChanged += Grid_SizeChanged;
+            cardMatchingGrid.CellClick += DataGridView_CellClick;
+            cardMatchingGrid.SizeChanged += Grid_SizeChanged;
 
-            this.Controls.Add(dataGridView);
+            this.panelCardGrid.Controls.Add(cardMatchingGrid);
 
             ResizeGridContent();
         }
@@ -48,15 +70,174 @@ namespace WinFormsMatchingGame
 
         private void ResizeGridContent()
         {
-            for (int i = 0; i < 4; ++i)
+            var gridDimensions = cardMatchingGrid.GridDimensions;
+
+            for (int i = 0; i < gridDimensions; ++i)
             {
-                dataGridView.Columns[i].Width = (this.ClientSize.Width / 4) - 1;
-                dataGridView.Rows[i].Height = (this.ClientSize.Height / 4) - 1;
+                cardMatchingGrid.Columns[i].Width = (this.panelCardGrid.ClientSize.Width / gridDimensions) - 1;
+                cardMatchingGrid.Rows[i].Height = (this.panelCardGrid.ClientSize.Height / gridDimensions) - 1;
             }
         }
 
         private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
+            var grid = sender as DataGridView;
+
+            // Take no action while the Try Again button is enabled.
+            if (buttonTryAgain.Enabled)
+            {
+                return;
+            }
+
+            // Take no action is the click is on a read-only cell.
+            if (this.cardMatchingGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly)
+            {
+                return;
+            }
+
+            var gridDimensions = cardMatchingGrid.GridDimensions;
+            var index = (gridDimensions * e.RowIndex) + e.ColumnIndex;
+
+            // Is this the first card turned over in an attempt to find a pair?
+            if (firstCardInPairAttempt == null)
+            {
+                firstCardInPairAttempt = (this.cardMatchingGrid.Rows[e.RowIndex].Cells[e.ColumnIndex] as DataGridViewButtonCellWithCustomName);
+                firstCardInPairAttempt.Style.BackColor = Color.Red;
+                
+                firstCardInPairAttempt.ReadOnly = true;
+            }
+            else 
+            {
+                // This must be the second card turned over in an attempt to find a pair.
+                // Has a pair been found?
+
+                var firstIndex = firstCardInPairAttempt.GetCardIndex();
+                var cardNameFirst = this.cardMatchingGrid.CardList[firstIndex].Name;
+
+                var cardNameSecond = this.cardMatchingGrid.CardList[index].Name;
+
+                secondCardInPairAttempt = (this.cardMatchingGrid.Rows[e.RowIndex].Cells[e.ColumnIndex] as DataGridViewButtonCellWithCustomName);
+
+                if (cardNameFirst == cardNameSecond)
+                {
+                    this.cardMatchingGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+
+                    firstCardInPairAttempt.Style.BackColor = Color.Green;
+                    this.cardMatchingGrid.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Green;
+
+                    this.cardMatchingGrid.CardList[index].Matched = true;
+                    var secondIndex = secondCardInPairAttempt.GetCardIndex();
+
+                    this.cardMatchingGrid.CardList[index].Matched = true;
+                    this.cardMatchingGrid.CardList[firstIndex].Matched = true;
+
+                    firstCardInPairAttempt = null;
+                    secondCardInPairAttempt = null;
+
+                    // Has the game been won?
+                    if (GameIsWon())
+                    {
+                        var answer = MessageBox.Show(
+                            this,
+                            "Congratulations, you won! Would you like another game?",
+                            "Accessible Matching Game",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question);
+                        if (answer == DialogResult.Yes)
+                        {
+                            RestartGame();
+                        }
+                    }
+
+                }
+                else
+                {
+                    secondCardInPairAttempt.ReadOnly = true;
+
+                    secondCardInPairAttempt = (this.cardMatchingGrid.Rows[e.RowIndex].Cells[e.ColumnIndex] as DataGridViewButtonCellWithCustomName);
+                    secondCardInPairAttempt.Style.BackColor = Color.Red;
+
+                    buttonTryAgain.Enabled = true;
+                }
+            }
+        }
+
+        private bool GameIsWon()
+        {
+            for (int i = 0; i < this.cardMatchingGrid.CardList.Count; i++)
+            {
+                if (!this.cardMatchingGrid.CardList[i].Matched)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void RestartGame()
+        {
+            // Todo: Get focus working back on the grid.
+            //this.cardMatchingGrid.Focus();
+
+            for (int i = 0; i < this.cardMatchingGrid.CardList.Count; i++)
+            {
+                this.cardMatchingGrid.CardList[i].Matched = false;
+            }
+
+            firstCardInPairAttempt = null;
+            secondCardInPairAttempt = null;
+
+            var gridDimensions = cardMatchingGrid.GridDimensions;
+
+            for (int r = 0; r < gridDimensions; r++)
+            {
+                for (int c = 0; c < gridDimensions; c++)
+                {
+                    var button = (this.cardMatchingGrid.Rows[r].Cells[c] as DataGridViewButtonCellWithCustomName);
+                    button.ReadOnly = false;
+                    button.Style.BackColor = Color.Gray;
+                }
+            }
+
+            buttonTryAgain.Enabled = false;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonTryAgain_Click(object sender, EventArgs e)
+        {
+            buttonTryAgain.Enabled = false;
+
+            firstCardInPairAttempt.Style.BackColor = Color.Gray;
+            secondCardInPairAttempt.Style.BackColor = Color.Gray;
+
+            firstCardInPairAttempt.ReadOnly = false;
+            secondCardInPairAttempt.ReadOnly = false;
+
+            firstCardInPairAttempt = null;
+            secondCardInPairAttempt = null;
+        }
+
+        private void buttonRestartGame_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+    }
+
+    public class CardMatchingGrid : DataGridView
+    {
+        public List<Card> CardList { get; set; }
+        public int CardCountInPlay { get; set; }
+        public int GridDimensions
+        {
+            get
+            {
+                return (int)Math.Sqrt(CardList.Count);
+            }
         }
     }
 
@@ -75,6 +256,12 @@ namespace WinFormsMatchingGame
             return new DataGridViewButtonCellWithCustomNameAccessibleObject(this);
         }
 
+        public int GetCardIndex()
+        {
+            var gridDimensions = (this.DataGridView as CardMatchingGrid).GridDimensions;
+            return ((gridDimensions * this.RowIndex) + this.ColumnIndex);
+        }
+
         protected class DataGridViewButtonCellWithCustomNameAccessibleObject : 
             DataGridViewButtonCellAccessibleObject
         {
@@ -86,8 +273,18 @@ namespace WinFormsMatchingGame
             {
                 get
                 {
-                    //return Resources.ResourceManager.GetString("ImageColumnAccessibleName") + base.Name;
-                    return "Face down";
+                    // Todo: Localize this.
+                    //return Resources.ResourceManager.GetString("Card")
+                    
+                    var cardName = "Card " + 
+                        ((this.Owner as DataGridViewButtonCellWithCustomName).GetCardIndex() + 1);
+
+                    // During app development, include the Value in the name.
+                    var index = (this.Owner as DataGridViewButtonCellWithCustomName).GetCardIndex();
+
+                    string fullName = cardName + ", " + this.Value;
+
+                    return fullName;
                 }
             }
 
@@ -96,11 +293,14 @@ namespace WinFormsMatchingGame
                 get
                 {
                     // The Name will contain all the data of interest to the customer.
-                    return null;
-                }
-                set
-                {
+                    var button = (this.Owner as DataGridViewButtonCellWithCustomName);
+                    var index = button.GetCardIndex();
+                    
+                    string value = button.ReadOnly ?
+                        (this.Owner.DataGridView as CardMatchingGrid).CardList[index].Name : 
+                        "Face down";
 
+                    return value;
                 }
             }
 
@@ -113,5 +313,11 @@ namespace WinFormsMatchingGame
             //    }
             //}
         }
+    }
+
+    public class Card
+    {
+        public string Name { get; set; }
+        public bool Matched { get; set; }
     }
 }
