cc73393f456af9d368381ba6341f9430d2d4320b
Guy Barker <77085891+gbarkerz@users.noreply.github.com>
2022-02-01 17:35:03 +0000

    Add screen reader details to the Help dialog.

--- WinFormsSquaresGame/HelpForm.Designer.cs
+++ WinFormsSquaresGame/HelpForm.Designer.cs
@@ -33,16 +33,19 @@ namespace WinFormsSquaresGame
             this.buttonClose = new System.Windows.Forms.Button();
             this.groupBoxKeyboardInstructions = new System.Windows.Forms.GroupBox();
             this.labelKeyboardInstructions = new System.Windows.Forms.Label();
+            this.groupBoxScreenReader = new System.Windows.Forms.GroupBox();
+            this.labelScreenReaderNotes = new System.Windows.Forms.Label();
             this.groupBoxKeyboardInstructions.SuspendLayout();
+            this.groupBoxScreenReader.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonClose
             // 
             this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonClose.Location = new System.Drawing.Point(534, 431);
+            this.buttonClose.Location = new System.Drawing.Point(534, 559);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(112, 34);
-            this.buttonClose.TabIndex = 0;
+            this.buttonClose.TabIndex = 2;
             this.buttonClose.Text = "&Close";
             this.buttonClose.UseVisualStyleBackColor = true;
             this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
@@ -53,7 +56,7 @@ namespace WinFormsSquaresGame
             this.groupBoxKeyboardInstructions.Location = new System.Drawing.Point(13, 13);
             this.groupBoxKeyboardInstructions.Name = "groupBoxKeyboardInstructions";
             this.groupBoxKeyboardInstructions.Size = new System.Drawing.Size(631, 401);
-            this.groupBoxKeyboardInstructions.TabIndex = 1;
+            this.groupBoxKeyboardInstructions.TabIndex = 0;
             this.groupBoxKeyboardInstructions.TabStop = false;
             this.groupBoxKeyboardInstructions.Text = "Keyboard Instructions";
             // 
@@ -62,17 +65,37 @@ namespace WinFormsSquaresGame
             this.labelKeyboardInstructions.AutoSize = true;
             this.labelKeyboardInstructions.Location = new System.Drawing.Point(19, 37);
             this.labelKeyboardInstructions.Name = "labelKeyboardInstructions";
-            this.labelKeyboardInstructions.Size = new System.Drawing.Size(593, 350);
+            this.labelKeyboardInstructions.Size = new System.Drawing.Size(598, 350);
             this.labelKeyboardInstructions.TabIndex = 0;
             this.labelKeyboardInstructions.Text = resources.GetString("labelKeyboardInstructions.Text");
+            // 
+            // groupBoxScreenReader
+            // 
+            this.groupBoxScreenReader.Controls.Add(this.labelScreenReaderNotes);
+            this.groupBoxScreenReader.Location = new System.Drawing.Point(13, 421);
+            this.groupBoxScreenReader.Name = "groupBoxScreenReader";
+            this.groupBoxScreenReader.Size = new System.Drawing.Size(631, 125);
+            this.groupBoxScreenReader.TabIndex = 1;
+            this.groupBoxScreenReader.TabStop = false;
+            this.groupBoxScreenReader.Text = "Screen Readers";
+            // 
+            // labelScreenReaderNotes
+            // 
+            this.labelScreenReaderNotes.AutoSize = true;
+            this.labelScreenReaderNotes.Location = new System.Drawing.Point(19, 31);
+            this.labelScreenReaderNotes.Name = "labelScreenReaderNotes";
+            this.labelScreenReaderNotes.Size = new System.Drawing.Size(600, 75);
+            this.labelScreenReaderNotes.TabIndex = 0;
+            this.labelScreenReaderNotes.Text = resources.GetString("labelScreenReaderNotes.Text");
             // 
             // HelpForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonClose;
-            this.ClientSize = new System.Drawing.Size(658, 477);
+            this.ClientSize = new System.Drawing.Size(658, 605);
             this.Controls.Add(this.groupBoxKeyboardInstructions);
+            this.Controls.Add(this.groupBoxScreenReader);
             this.Controls.Add(this.buttonClose);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -82,6 +105,8 @@ namespace WinFormsSquaresGame
             this.Text = "Squares Game Help";
             this.groupBoxKeyboardInstructions.ResumeLayout(false);
             this.groupBoxKeyboardInstructions.PerformLayout();
+            this.groupBoxScreenReader.ResumeLayout(false);
+            this.groupBoxScreenReader.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -91,5 +116,7 @@ namespace WinFormsSquaresGame
         private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.GroupBox groupBoxKeyboardInstructions;
         private System.Windows.Forms.Label labelKeyboardInstructions;
+        private System.Windows.Forms.GroupBox groupBoxScreenReader;
+        private System.Windows.Forms.Label labelScreenReaderNotes;
     }
 }
